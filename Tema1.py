27acf01af401f28e5ddffa0c49f654ae272574a2
Iulia Chirovici <iulia.chirovici@ebs.msft>
2020-06-25 17:25:45 +0300

    Tema 1

---
+++ Tema1.py
@@ -0,0 +1,35 @@
+# 1. Reverse the order of the items in an array.
+# Example:
+# a = [1, 2, 3, 4, 5]
+# Result:
+# a = [5, 4, 3, 2, 1]
+
+array = [1, 2, 3, 4, 5]
+print('Array-ul este: ', array)
+x = len(array)
+print('Lungimea array-ului este: ', x)
+print('Array-ul inversat este: ', array[::-1])
+
+# 2. Get the number of occurrences of var b in array a.
+# Example:
+# a = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+# b = 2
+# Result:
+# 4
+
+arr = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b = 2
+x = arr.count(b)
+print('Numarul de aparitii al lui b este: ', x)
+
+# 3. Given a sentence as string, count the number of words in it.
+# Example:
+# a = 'ana are mere si nu are pere'
+# Result:
+# 7
+
+a = 'ana are mere si nu are pere'
+b = a.split(' ')
+x = len(b)
+print('Nr. de cuvinte este ', x)
+
